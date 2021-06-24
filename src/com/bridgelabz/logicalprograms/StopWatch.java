7e4fd3a3c56a35d9209b2aa3bfa95c71daddc4e8
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-24 15:05:35 +0530

    Added:StopWatch class and calculated the elapsed time.

--- src/com/bridgelabz/logicalprograms/StopWatch.java
+++ src/com/bridgelabz/logicalprograms/StopWatch.java
@@ -1,6 +1,10 @@
 package com.bridgelabz.logicalprograms;
-
 public class StopWatch {
+    /**
+     * calling currentTimeMillis  method and printing start time and end time and calling printElapsedTime method
+     * by passing startTime and endTime as parameters.
+     * @param args
+     */
     public static void main(String[] args){
         long startTime=System.currentTimeMillis();
         System.out.println("start time is "+startTime+" milli seconds");
@@ -9,11 +13,19 @@ public class StopWatch {
         System.out.println("endTime is"+endTime+" milli seconds");
         printElapsedTime(startTime,endTime);
     }
+    /**
+     * Printing the elapsed time.
+     * @param startTime
+     * @param endTime
+     */
     private static void printElapsedTime(long startTime,long endTime)
     {
         long elapsedTime=endTime-startTime;
         System.out.println("Elapsed time:"+elapsedTime+" milli seconds");;
     }
+    /**
+     * Just printing a simple statements until given condition.
+     */
     public static void printHelloWorld()
     {
         for(int i=0;i<10;i++)
