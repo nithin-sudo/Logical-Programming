ea145344caeed383b16646dfde394e46b5ee9c9c
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-24 12:31:15 +0530

    Added:Fibonacci class and printing fibonacci series until given range.

---
+++ src/com/bridgelabz/logicalprograms/Fibonacci.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.logicalprograms;
+import java.util.Scanner;
+public class Fibonacci {
+    /**
+     * Taking the fibonacciRange input from user and printing fibonacci series until that range.
+     * @param args
+     */
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a range: ");
+        int fibonacciRange = sc.nextInt();
+        int precedingNumberOne=0,precedingNumberTwo=1,nextNumber;
+        System.out.println("fibonnaci series for given range:");
+        System.out.print(precedingNumberOne);
+        System.out.print(" "+precedingNumberTwo);
+        for(int i=2;i<fibonacciRange;i++)
+        {
+            nextNumber=precedingNumberOne+precedingNumberTwo;
+            precedingNumberOne=precedingNumberTwo;
+            precedingNumberTwo=nextNumber;
+            System.out.print(" "+nextNumber);
+        }
+
+    }
+}
